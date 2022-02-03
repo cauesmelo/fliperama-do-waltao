90a489c2427575b0670b34dee048ad59f08ef903
Cauê Melo <caue.melo@impar.com.br>
2022-02-02 22:21:19 -0300

    changes

--- gameConstants.js
+++
@@ -1,4 +0,0 @@
-export const primaryColor = "#093d1a";
-export const backgroundColor = "#000000";
-export const windowHeight = 320;
-export const windowWidth = 480;
--- script.js
+++ script.js
@@ -1,18 +1,49 @@
-// Importa classe da bola
-import { Ball } from "./breakout/Ball.js";
-import { Paddle } from "./breakout/Paddle.js";
+// Constantes gerais
+const primaryColor = "#093d1a";
+const backgroundColor = "#000000";
+const windowHeight = 320;
+const windowWidth = 480;
 
-// Cores
-import {
-  primaryColor,
-  backgroundColor,
-  windowHeight,
-  windowWidth,
-} from "./gameConstants.js";
+// ====Código compartilhado
+// Estado geral
+let selectedGame = "Breakout";
+let isGameOver = false;
+let isGameWin = false;
+let isGameStarted = false;
+let score = 0;
+
+// Handler de game select
+const handleChangeGame = (game) => (selectedGame = game);
 
 // Pega canvas e contexto
-const canvas = document.getElementById("breakout");
+const canvas = document.getElementById("gameCanvas");
+canvas.width = windowWidth;
+canvas.height = windowHeight;
 const ctx = canvas.getContext("2d");
+
+// Helper para escrita em tela
+const write = (text, x, y, fontSize) => {
+  if (fontSize) {
+    ctx.font = `${fontSize}px 'Press Start 2P'`;
+  }
+  ctx.fillText(text, x, y);
+  ctx.font = "12px 'Press Start 2P'";
+};
+
+const writeLeft = (text, x, y, fontSize) => {
+  ctx.textAlign = "left";
+  if (fontSize) {
+    ctx.font = `${fontSize}px 'Press Start 2P'`;
+  }
+  ctx.fillText(text, x, y);
+  ctx.font = "12px 'Press Start 2P'";
+  ctx.textAlign = "center";
+};
+
+// Configurações padrão para escrita em tela
+ctx.font = "12px 'Press Start 2P'";
+ctx.fillStyle = primaryColor;
+ctx.textAlign = "center";
 
 // Guarda tempo para calcular o delta time
 let lastUpdate = Date.now();
@@ -44,7 +75,7 @@ const keyDownHandler = (e) => {
     case "ArrowDown":
       downPressed = true;
       break;
-    case "Space":
+    case " ":
       spacePressed = true;
       break;
   }
@@ -68,7 +99,7 @@ const keyUpHandler = (e) => {
     case "ArrowDown":
       downPressed = false;
       break;
-    case "Space":
+    case " ":
       spacePressed = false;
       break;
   }
@@ -78,10 +109,6 @@ const keyUpHandler = (e) => {
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 
-// Cria objetos
-const ball = new Ball(180, 160, ctx);
-const paddle = new Paddle(windowWidth / 2, windowHeight - 10, ctx);
-
 // Função para calcular delta time
 const calculateDeltaTime = () => {
   let now = Date.now();
@@ -89,22 +116,255 @@ const calculateDeltaTime = () => {
   lastUpdate = now;
 };
 
-// Game loop
+// =======BREAKOUT CODE
+// ===Classe da bolinha
+class Ball {
+  constructor(x, y, ctx) {
+    this.x = x;
+    this.y = y;
+    this.height = 10;
+    this.width = 10;
+    this.ctx = ctx;
+    this.velocityX = 0;
+    this.velocityY = 0;
+  }
+
+  draw() {
+    this.ctx.beginPath();
+    this.ctx.rect(this.x, this.y, this.width, this.height);
+    this.ctx.fillStyle = primaryColor;
+    this.ctx.fill();
+    this.ctx.closePath();
+  }
+
+  move() {
+    this.x += this.velocityX * deltaTime;
+    this.y += this.velocityY * deltaTime;
+
+    if (this.checkCollisionWithPaddle()) {
+      this.velocityY *= -1;
+      this.increaseVelocity();
+      return;
+    }
+
+    checkCollisionWithBricks();
+
+    if (this.x <= 0) {
+      this.velocityX *= -1;
+    }
+
+    if (this.x >= windowWidth - this.width) {
+      this.velocityX *= -1;
+    }
+
+    if (this.y <= 0) {
+      this.velocityY *= -1;
+    }
+
+    if (this.y > windowHeight - this.height) {
+      isGameOver = true;
+    }
+  }
+
+  checkCollisionWithPaddle() {
+    if (this.x >= paddle.x && this.x <= paddle.x + paddle.width) {
+      if (this.y >= paddle.y - this.height) {
+        return true;
+      }
+    }
+  }
+
+  checkCollisionWithBricks() {
+    for (var c = 0; c < bricks.columns; c++) {
+      for (var r = 0; r < bricks.rows; r++) {
+        var b = bricks.bricks[c][r];
+        if (!b.destroyed) {
+          if (
+            x > b.x &&
+            x < b.x + brickWidth &&
+            y > b.y &&
+            y < b.y + brickHeight
+          ) {
+            dy = -dy;
+            b.destroyed = true;
+          }
+        }
+      }
+    }
+  }
+
+  increaseVelocity() {
+    this.velocityX += this.velocityX > 0 ? 0.1 : -0.1;
+    this.velocityY += this.velocityY > 0 ? 0.1 : -0.1;
+  }
+
+  // Seta posição da bolinha manualmente
+  setPosition(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  // Lança a bolinha
+  launch() {
+    isGameStarted = true;
+    this.velocityY = -1;
+
+    // Se o botão da direita tiver pressionado, lança pra direita
+    // Se não, se o botão da esquerda tiver presssionado, lança pra esquerda
+    // Se não, lança aleatoriamente para um dos lados
+    this.velocityX = rightPressed
+      ? Math.random() * (1 - 0.7) + 0.7
+      : leftPressed
+      ? Math.random() * (1 - 0.7) + 0.7 * -1
+      : Math.random() < 0.5
+      ? Math.random() * (1 - 0.7) + 0.7 * -1
+      : Math.random() * (1 - 0.7) + 0.7;
+  }
+}
+
+// ===Classe da raquete
+class Paddle {
+  constructor(x, y, ctx) {
+    this.x = x;
+    this.y = y;
+    this.height = 10;
+    this.width = 70;
+    this.ctx = ctx;
+  }
+
+  // Desenha o Paddle na tela
+  draw() {
+    this.ctx.beginPath();
+    this.ctx.rect(this.x, this.y, this.width, this.height);
+    this.ctx.fillStyle = primaryColor;
+    this.ctx.fill();
+    this.ctx.closePath();
+  }
+
+  // Move para esquerda, sempre multiplicando pela delta time para
+  // não haver diferença de velocidade com mudança de fps
+  moveLeft() {
+    if (this.x > 0) this.x -= 5 * deltaTime;
+  }
+
+  // Move para direita
+  moveRight() {
+    if (this.x < windowWidth - this.width) this.x += 5 * deltaTime;
+  }
+}
+
+var brickRowCount = 3;
+var brickColumnCount = 5;
+var brickWidth = 75;
+var brickHeight = 20;
+var brickPadding = 10;
+var brickOffsetTop = 30;
+var brickOffsetLeft = 30;
+
+// ===Classe de brick
+class Bricks {
+  constructor() {
+    this.rows = 3;
+    this.columns = 5;
+    this.width = 70;
+    this.height = 20;
+    this.bricks = [];
+
+    for (let c = 0; c < this.columns; c++) {
+      this.bricks[c] = [];
+      for (let r = 0; r < this.rows; r++) {
+        this.bricks[c][r] = { x: 0, y: 0 };
+      }
+    }
+  }
+
+  draw() {
+    for (let c = 0; c < this.columns; c++) {
+      for (let r = 0; r < this.rows; r++) {
+        if (!this.bricks[c][r].destroyed) {
+          let brickX = c * (this.width + 10) + 45;
+          let brickY = r * (this.height + 10) + 40;
+          this.bricks[c][r].x = brickX;
+          this.bricks[c][r].y = brickY;
+          ctx.beginPath();
+          ctx.rect(brickX, brickY, this.width, this.height);
+          ctx.fillStyle = primaryColor;
+          ctx.fill();
+          ctx.closePath();
+        }
+      }
+    }
+  }
+}
+
+// Cria objetos
+const ball = new Ball(180, 160, ctx);
+const paddle = new Paddle(windowWidth / 2, windowHeight - 10, ctx);
+const bricks = new Bricks();
+
+// Gameloop do breakout
+const breakoutGameLoop = () => {
+  if (isGameOver) {
+    write("Você perdeu :(", canvas.width / 2, canvas.height / 2, 16);
+    write(
+      "Pressione espaço para reiniciar",
+      canvas.width / 2,
+      canvas.height / 2 + 30
+    );
+
+    if (spacePressed) {
+      spacePressed = false;
+      isGameOver = false;
+      isGameStarted = false;
+    }
+  } else {
+    if (rightPressed) {
+      paddle.moveRight();
+    }
+
+    if (leftPressed) {
+      paddle.moveLeft();
+    }
+
+    if (isGameStarted) {
+      ball.move();
+    } else {
+      ball.setPosition(paddle.x + paddle.width / 2 - 5, paddle.y - 10);
+      if (spacePressed) {
+        ball.launch();
+      }
+    }
+
+    ball.draw();
+    paddle.draw();
+    bricks.draw();
+  }
+
+  writeLeft("Score:" + score, 10, 30, 15);
+};
+
+// ===Game loop
 const gameLoop = () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   calculateDeltaTime();
 
-  if (rightPressed) {
-    paddle.moveRight(deltaTime);
+  switch (selectedGame) {
+    case "Breakout":
+      breakoutGameLoop();
+      break;
+    case "Corona Invaders":
+      // TODO
+      break;
+    case "Snake":
+      // TODO
+      break;
+    case "Pong 360":
+      // TODO
+      break;
+    case "TicTacToe":
+      // TODO
+      break;
   }
-
-  if (leftPressed) {
-    paddle.moveLeft(deltaTime);
-  }
-
-  ball.move(deltaTime);
-  ball.draw();
-  paddle.draw();
 };
 
 // Configura game loop para rodar a 60fps
